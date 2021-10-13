df65a5bcf394156574359476e138b6f59b8e6b6e
JackyWeng526 <63989896+JackyWeng526@users.noreply.github.com>
2021-10-13 11:25:03 +0800

    Add files via upload

--- src/CWB_downloader.py
+++ src/CWB_downloader.py
@@ -1,4 +1,3 @@
-#%% 
 # Import pakages
 import pandas as pd
 import numpy as np
@@ -12,47 +11,50 @@ BATH_PATH = os.path.dirname(os.path.abspath(__file__))
 etc_PATH = os.path.join(BATH_PATH, "..", "etc")
 data_PATH = os.path.join(BATH_PATH, "..", "data")
 
-# Import station information of Central Weather Bureau Taiwan
-CWB_meta = json.load(open(os.path.join(etc_PATH, "CWB_meta.json")))
-CWB_meta_df = pd.DataFrame.from_dict(CWB_meta[0])
-CWB_meta_df
 
-# %% 
-# Gather hourly historical data of Taipei station
-# Extract station number, station name, and datetime (Mandarin characters must be transcoded twice with urllib)
-stnum = CWB_meta_df.iloc[4, 0]
-stname = urllib.parse.quote(urllib.parse.quote(CWB_meta_df.iloc[4, 1]))
-date = datetime.date(2020, 1, 1)
+class CWB_downloader:
+    def __init__(self, station_number, date_start, date_end):
+        self.stn = station_number
+        self.date_start = date_start
+        self.date_end = date_end
+        self.st_name = self.CWB_station_name()
+        self.time_period = self.download_time_period()
 
-# Take 2020-01-01 as example
-html_data = pd.read_html(F"https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&station={stnum}&stname={stname}&datepicker={date}")
-data_table = html_data[1]
-data_table
+    def CWB_station_name(self):
+        # Get station name from station information of Central Weather Bureau Taiwan
+        CWB_meta = json.load(open(os.path.join(etc_PATH, "CWB_meta.json")))
+        CWB_meta_df = pd.DataFrame.from_dict(CWB_meta[0])
+        st_name = CWB_meta_df.loc[CWB_meta_df["站號"]==self.stn, "站名"].values[0]
+        return st_name
 
-# %%
-# Drop duplicate columns
-data_table.columns = [i[2] for i in np.array(data_table.columns)]
+    def download_time_period(self):
+        date_start = self.date_start
+        date_end = self.date_end
+        date_delta = (date_end - date_start).days + 1
+        return date_delta
 
-# Insert datetime value
-data_table.insert(loc=0, column="Date", value=date)
-data_table
+    def data_downloader(self):
+        cwb_data = []
+        date_start = self.date_start
+        date_delta = self.time_period
+        for d in range(date_delta):
+            date = date_start + datetime.timedelta(days=d)
+            html_df = pd.read_html(F"https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&station=466920&stname=%25E8%2587%25BA%25E5%258C%2597&datepicker={date}")[1]
+            html_df.columns = [i[2] for i in np.array(html_df.columns)]
+            html_df.insert(loc=0, column="Date", value=date)
+            cwb_data.append(html_df)
+        _df = pd.concat(cwb_data, axis=0)
+        print(F"{self.st_name} weather data between {self.date_start} and {self.date_end} has been downloaded!")
+        return _df        
 
-# %% 
-%%time
-# Use for-loop to download daily historical data of Taipei station
-cwb_data = []
-date_start = datetime.date(2020, 1, 1)
-for i in range(366):
-    date = date_start + datetime.timedelta(days=i)
-    html_df = pd.read_html(F"https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&station=466920&stname=%25E8%2587%25BA%25E5%258C%2597&datepicker={date}")[1]
-    html_df.columns = [i[2] for i in np.array(html_df.columns)]
-    html_df.insert(loc=0, column="Date", value=date)
-    cwb_data.append(html_df)
-_df = pd.concat(cwb_data, axis=0)
-_df
+    def data_saver(self):
+        _df = self.data_downloader()
+        _df.to_csv(os.path.join(data_PATH, F"{self.st_name}_{self.date_start}_{self.date_end}.csv"))
+        print("Weather data has been saved!")
 
-# %% 
-# Save dataframe as csv file for advanced analysis and application
-_df.to_csv(os.path.join(data_PATH, "Taipei_2020.csv"))
-print("Taipei weather data in 2020 has been saved!")
-# %%
+
+if __name__ == "__main__":
+    station_number = 466920
+    date_start = datetime.date(2020, 1, 1)
+    date_end = datetime.date(2020, 1, 2)
+    CWB_downloader(station_number, date_start, date_end).data_saver()
